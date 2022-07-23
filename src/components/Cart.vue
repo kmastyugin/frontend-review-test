d96e506cab17b835a63405ad493d00b678a06a02
Kirill Mastyugin <kirs_mast@mail.ru>
2022-07-23 12:57:21 +0300

    Комментарии по коду и замечания

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -7,18 +7,26 @@
 </template>
 
 <script>
+/**
+ * 15. spisok_dlya_rendera - великолепное название))) 
+ * 
+ * 16. Не хватает важного атрибута key, который служит для оптимизации реактивной отрисовки состояния(массива)
+ * 
+ * 17. Избавляемся от пропса cart, получаем список корзины из стора.
+ * 
+ * 18. Меняем итерируемый div, На тег li, который оборачиваем в ul, дабы это стало семантически списком.
+ */
 export default {
   props: {
     cart: Array,
   },
   computed: {
-      spisok_dlya_rendera() {
-          return this.cart.reverse();
-      },
+    spisok_dlya_rendera() {
+      return this.cart.reverse();
+    },
   },
 };
 </script>
 
 <style>
-
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,25 @@
-import api from '../../api/products.js';
-import Vue from 'vue';
-import Vuex from 'vuex';
+import api from "../../api/products.js";
+import Vue from "vue";
+import Vuex from "vuex";
 Vue.use(Vuex);
 
+/**
+ * 19. Избавляемся свойств state, mutations, actions и декомпозируем логику по соответствующим модулям.
+ *
+ * 20. Модуль cart - управляющий корзиной
+ * Модуль currency - управляющий выбранной валютой
+ * Модуль products - управляющий списокм товаров
+ *
+ * 21. Импортируем модули стора, добалвяем в объект modules.
+ */
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
+  state: {},
+  mutations: {},
   actions: {
     getProductsList(ctx, data) {
       return api.getProductsList();
     },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
